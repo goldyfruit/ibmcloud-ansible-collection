04113c63a8702ac8b74f3860e2a345f7a2d9f2d8
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-08 00:09:18 -0400

    [ic_is_instance_fip_info] Add filter by floating IP

--- ic_is_instance_fip_info.py
+++ ic_is_instance_fip_info.py
@@ -21,7 +21,7 @@ author: Gaëtan Trellu (@goldyfruit)
 version_added: "2.9"
 description:
     - Retrieve floating IPs attached to VSI (Virtual Server
-      Instance) on IBM Cloud.
+      Instance) network interfaces on IBM Cloud.
 notes:
     - The result contains a list of floating IPs.
 requirements:
@@ -31,21 +31,30 @@ options:
         description:
             - Instance UUID or name.
         required: true
+    fip:
+        description:
+            - Floating IP name, ID or address.
+        required: false
 extends_documentation_fragment:
     - ibmcloud
 '''
 
 EXAMPLES = '''
-# Retrieve floating IPs attached to the VSI
+# Retrieve floating IPs attached to a VSI
 - ic_is_instance_fip_info:
     instance: ibmcloud-vsi-baby
+
+# Retrieve specific floating IP attached to a VSI
+- ic_is_instance_fip_info:
+    instance: ibmcloud-vsi-baby
+    floating_ip: 192.234.192.234
 
 # Retrieve floating IPs and register the value
 - ic_is_instance_fip_info:
     instance: ibmcloud-vsi-baby
   register: fips
 
-# Display config registered value
+# Display fips registered value
 - debug:
     var: fips
 '''
@@ -56,6 +65,9 @@ def run_module():
         instance=dict(
             type='str',
             required=True),
+        floating_ip=dict(
+            type='str',
+            required=False),
     )
 
     module = AnsibleModule(
@@ -66,18 +78,29 @@ def run_module():
     instance = sdk.Instance()
 
     name = module.params['instance']
+    floating_ip = module.params['floating_ip']
 
     interfaces = instance.get_instance_interfaces(name)
     if "errors" in interfaces:
-        module.fail_json(msg=interfaces)
+        module.fail_json(msg=interfaces["errors"])
 
     nics = []
     fips = {}
     for interface in interfaces["network_interfaces"]:
         data = instance.get_instance_interface_fips(name, interface["id"])
         if "errors" in data:
-            module.fail_json(msg=data)
+            module.fail_json(msg=data["errors"])
+
+        if floating_ip:
+            fip = instance.get_instance_interface_fip(name, interface["id"],
+                                                      floating_ip)
+            if "errors" in data:
+                module.fail_json(msg=fip["errors"])
+
+            module.exit_json(**fip)
+
         nics.append(data)
+
     fips["nics"] = nics
 
     module.exit_json(**fips)
