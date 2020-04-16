6ee765f6bff31c93815a206a661a8adf55debf69
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-16 11:09:35 -0400

    [ic_is_gateway_info] Update documentation

--- ic_is_gateway_info.py
+++ ic_is_gateway_info.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 from ansible.module_utils.basic import AnsibleModule
@@ -13,41 +15,33 @@ ANSIBLE_METADATA = {
     'supported_by': 'community'
 }
 
-DOCUMENTATION = '''
+DOCUMENTATION = r'''
 ---
 module: ic_is_gateway_info
-short_description: Retrieve information about public gateways.
+short_description: Retrieve VPC public gateway on IBM Cloud.
 author: Gaëtan Trellu (@goldyfruit)
 version_added: "2.9"
 description:
-    - Retrieve information about public gateways from IBM Cloud.
+  - A public gateway is a virtual network device associated with a VPC,
+    which allows access to the Internet. A public gateway resides in a
+    zone and can be connected to subnets in the same zone only.
 notes:
-    - The result contains a list of public gateways.
+  - The result contains a list of public gateways.
 requirements:
-    - "ibmcloud-python-sdk"
+  - "ibmcloud-python-sdk"
 options:
-    gateway:
-        description:
-            - Restrict results to public gateway with UUID or name matching.
-        required: false
-extends_documentation_fragment:
-    - ibmcloud
+  gateway:
+    description:
+      - Restrict results to public gateway with ID or name matching.
+    type: str
 '''
 
-EXAMPLES = '''
-# Retrieve public gateway list
-- ic_is_gateway_info:
+EXAMPLES = r'''
+- name: Retrieve public gateway list
+  ic_is_gateway_info:
 
-# Retrieve public gateway list and register the value
-- ic_is_gateway_info:
-  register: gateways
-
-# Display gateways registered value
-- debug:
-    var: gateways
-
-# Retrieve a specific public gateway by ID or by name
-- ic_is_gateway_info:
+- name: Retrieve specific public gateway
+  ic_is_gateway_info:
     gateway: ibmcloud-gateway-baby
 '''
 
@@ -64,16 +58,16 @@ def run_module():
         supports_check_mode=False
     )
 
-    gateway = sdk.Gateway()
+    public_gateway = sdk.Gateway()
 
-    name = module.params['gateway']
+    gateway = module.params['gateway']
 
-    if name:
-        result = gateway.get_public_gateway(name)
+    if gateway:
+        result = public_gateway.get_public_gateway(gateway)
         if "errors" in result:
             module.fail_json(msg=result["errors"])
     else:
-        result = gateway.get_public_gateways()
+        result = public_gateway.get_public_gateways()
         if "errors" in result:
             module.fail_json(msg=result["errors"])
 
