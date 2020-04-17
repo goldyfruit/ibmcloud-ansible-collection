55f1804c4e029543d2d68385e46db0cfc9fa0855
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-17 16:24:52 -0400

    [ic_is_lb_rule_info] Documentation update

--- ic_is_lb_rule_info.py
+++ ic_is_lb_rule_info.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python
 
 # GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 from ansible.module_utils.basic import AnsibleModule
@@ -15,61 +16,49 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = r'''
 ---
 module: ic_is_lb_rule_info
-short_description: Retrieve information about rules from load balancer.
+short_description: Retrieve VPC load balancer rules on IBM Cloud.
 author: Gaëtan Trellu (@goldyfruit)
 version_added: "2.9"
 description:
-    - Retrieve information about rules within policy from listeners for
-      specific load balancers from IBM Cloud.
+  - Retrieves a list of all rules belonging to the load balancer listener
+    policy.
 notes:
-    - The result contains a list of rules.
+  - The result contains a list of rules.
 requirements:
-    - "ibmcloud-python-sdk"
+  - "ibmcloud-python-sdk"
 options:
-    lb:
-      description:
-        - Load balancer name or ID.
+  lb:
+    description:
+      - Load balancer name or ID.
+      type: str
       required: true
-    listener:
-      description:
-        - Restrict results to listener with ID or port matching.
-      required: false
-    port:
-      description:
-        - Restrict results to listener with port matching.
-      required: false
-    policy:
-      description:
-        - Restrict results to policy with name or ID matching.
-      required: true
-    rule:
-      description:
-        - Restrict results to rule with name or ID matching.
-    required: false
+  listener:
+    description:
+      - Listener port or ID
+    type: str
+    required: true
+  policy:
+    description:
+      - Policy name or ID
+    type: str
+    required: true
+  rule:
+    description:
+      - Restrict results to rule with name or ID matching.
+    type: str
 '''
 
 EXAMPLES = r'''
-# Retrieve rule list from specific policy
-- ic_is_lb_rule_info:
+- name: Retrieve rule list
+  ic_is_lb_rule_info:
     lb: ibmcloud-lb-baby
     listener: r006-e503af14-7ca1-4bb6-a8eb-f25b73323041
     policy: ibmcloud-lb-policy-baby
 
-# Retrieve rule list and register the value
-- ic_is_lb_rule_info:
+- name: Retrieve specific rule
+  ic_is_lb_rule_info:
     lb: ibmcloud-lb-baby
     listener: r006-e503af14-7ca1-4bb6-a8eb-f25b73323041
-    policy: ibmcloud-lb-policy-baby
-  register: rules
-
-# Display rules registered value
-- debug:
-    var: rules
-
-# Retrieve specific policy from listener (port filtering)
-- ic_is_lb_rule_info:
-    lb: ibmcloud-lb-baby
-    port: 443
     policy: ibmcloud-lb-policy-baby
     rule: ibmcloud-lb-rule-baby
 '''
@@ -82,10 +71,7 @@ def run_module():
             required=True),
         listener=dict(
             type='str',
-            required=False),
-        port=dict(
-            type='int',
-            required=False),
+            required=True),
         policy=dict(
             type='str',
             required=True),
@@ -102,26 +88,20 @@ def run_module():
     loadbalancer = sdk.Loadbalancer()
 
     lb = module.params['lb']
-    id = module.params['listener']
-    port = module.params['port']
+    listener = module.params['listener']
     policy = module.params['policy']
-    name = module.params['rule']
+    rule = module.params['rule']
 
-    if id:
-        listener = id
-    elif port:
-        listener = int(port)
-
-    if name:
+    if rule:
         result = loadbalancer.get_lb_listener_policy_rule(lb, listener, policy,
-                                                          name)
+                                                          rule)
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
     else:
         result = loadbalancer.get_lb_listener_policy_rules(lb, listener,
                                                            policy)
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
 
     module.exit_json(**result)
 
