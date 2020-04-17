ab15ec8c8816668f374e87d8ed0e1c1e41b6f5bf
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-17 16:10:05 -0400

    [ic_is_lb_pool_info] Documentation update

--- ic_is_lb_pool_info.py
+++ ic_is_lb_pool_info.py
@@ -1,10 +1,12 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # GNU General Public License v3.0+
 
 
 from ansible.module_utils.basic import AnsibleModule
 from ibmcloud_python_sdk.vpc import loadbalancer as sdk
+
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -15,45 +17,36 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = r'''
 ---
 module: ic_is_lb_pool_info
-short_description: Retrieve information about pools within load balancer.
+short_description: Retrieve VPC load balancer pools on IBM Cloud.
 author: Gaëtan Trellu (@goldyfruit)
 version_added: "2.9"
 description:
-    - Retrieve information about pools within specific load balancers from
-      IBM Cloud.
+  - This module lists all pools that belong to the load balancer.
 notes:
-    - The result contains a list of pools.
+  - The result contains a list of pools.
 requirements:
-    - "ibmcloud-python-sdk"
+  - "ibmcloud-python-sdk"
 options:
-    lb:
-      description:
-        - Load balancer name or ID.
-      required: true
-    pool:
-      description:
-        - Restrict results to pool with UUID or name matching.
-      required: false
+  lb:
+    description:
+      - Load balancer name or ID.
+    type: str
+    required: true
+  pool:
+    description:
+      - Restrict results to pool with ID or name matching.
+    type: str
 '''
 
 EXAMPLES = r'''
-# Retrieve pool list specific load balancer
-- ic_is_lb_pool_info:
+- name: Retrieve pool list from load balancer
+  ic_is_lb_pool_info:
     lb: ibmcloud-lb-baby
 
-# Retrieve pool list and register the value
-- ic_is_lb_pool_info:
+- name: Retrieve specific pool from load balancer
+  ic_is_lb_info:
     lb: ibmcloud-lb-baby
-  register: pools
-
-# Display pools registered value
-- debug:
-    var: pools
-
-# Retrieve specific pool from a load balancer
-- ic_is_lb_info:
-    lb: ibmcloud-lb-baby
-    pool: ibmcloud-pool-baby
+    pool: ibmcloud-lb-pool-baby
 '''
 
 
@@ -75,16 +68,16 @@ def run_module():
     loadbalancer = sdk.Loadbalancer()
 
     lb = module.params['lb']
-    name = module.params['pool']
+    pool = module.params['pool']
 
-    if name:
-        result = loadbalancer.get_lb_pool(lb, name)
+    if pool:
+        result = loadbalancer.get_lb_pool(lb, pool)
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
     else:
         result = loadbalancer.get_lb_pools(lb)
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
 
     module.exit_json(**result)
 
