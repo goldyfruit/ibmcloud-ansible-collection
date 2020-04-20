60b809f4fdd5dfc3754bd9c77bf9d8aec98d3897
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-20 17:29:24 -0400

    [ic_is_vpn_ipsec_info] Documentation update

--- ic_is_vpn_ipsec_info.py
+++ ic_is_vpn_ipsec_info.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 from ansible.module_utils.basic import AnsibleModule
@@ -16,11 +18,12 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = r'''
 ---
 module: ic_is_vpn_ipsec_info
-short_description: Retrieve information about VPN IPsec policy.
+short_description: Manage VPC VPN IPsec policies on IBM Cloud.
 author: Gaëtan Trellu (@goldyfruit)
 version_added: "2.9"
 description:
-  - Retrieve information about VPN IPsec policies from IBM Cloud.
+  - This module retrieves a paginated list of all IPsec policies that belong
+    to this account.
 notes:
   - The result contains a list of policies.
 requirements:
@@ -29,23 +32,16 @@ options:
   policy:
     description:
       - Restrict results to VPN IPsec policy with ID or name matching.
+    type: str
 '''
 
 EXAMPLES = r'''
-# Retrieve VPN IPsec policy list
-- ic_is_vpn_ipsec_info:
+- name: Retrieve VPN IPsec policy list
+  ic_is_vpn_ipsec_info:
 
-# Retrieve VPN IPsec policy list and register the value
-- ic_is_vpn_ipsec_info:
-  register: policies
-
-# Display VPN IPsec policies registered value
-- debug:
-    var: policies
-
-# Retrieve a specific VPN IPsec policy
-- ic_is_vpn_ipsec_info:
-    policy: ibmcloud-vpn-ipsec-policy-baby
+- name: Retrieve a specific VPN IPsec policy
+  ic_is_vpn_ipsec_info:
+    policy: ibmcloud-vpn-ipsec-baby
 '''
 
 
@@ -63,16 +59,16 @@ def run_module():
 
     vpn = sdk.Vpn()
 
-    name = module.params['policy']
+    policy = module.params['policy']
 
-    if name:
-        result = vpn.get_ipsec_policy(name)
+    if policy:
+        result = vpn.get_ipsec_policy(policy)
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
     else:
         result = vpn.get_ipsec_policies()
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
 
     module.exit_json(**result)
 
