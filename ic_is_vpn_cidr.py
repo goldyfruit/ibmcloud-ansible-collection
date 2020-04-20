4c30f6f1281a47850bc5ac6a8333b84955299db1
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-04-20 17:01:08 -0400

    [ic_is_vpn_cidr] Small refact and documentation update

--- ic_is_vpn_cidr.py
+++ ic_is_vpn_cidr.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
 
 
 from ansible.module_utils.basic import AnsibleModule
@@ -16,11 +18,13 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = r'''
 ---
 module: ic_is_vpn_cidr
-short_description: Add or remove CIDR from VPN connection.
+short_description: Manage VPC VPN local or peer CIDRs on IBM Cloud.
 author: Gaëtan Trellu (@goldyfruit)
 version_added: "2.9"
 description:
-  - Add or remove local/peer CIDR from VPN connection on IBM Cloud.
+  - This module adds the specified CIDR to the specified resource. A request
+    body is not required, and if supplied, is ignored. This request succeeds
+    if the CIDR already exists on the resource.
 requirements:
   - "ibmcloud-python-sdk"
 options:
@@ -49,35 +53,35 @@ options:
     description:
       - Should the resource be present or absent.
     type: str
-    choices: [present, absent]
     default: present
+    choices: [present, absent]
 '''
 
 EXAMPLES = r'''
-# Add peer CIDR to VPN connection
-- ic_is_vpn_cidr:
+- name: Add peer CIDR to VPN connection
+  ic_is_vpn_cidr:
     gateway: ibmcloud-vpn-gateway-baby
     connection: ibmcloud-vpn-connection-baby
     target: peer
     cidr: 10.0.0.0/24
 
-# Add local CIDR to VPN connection
-- ic_is_vpn_cidr:
+- name: Add local CIDR to VPN connection
+  ic_is_vpn_cidr:
     gateway: ibmcloud-vpn-gateway-baby
     connection: ibmcloud-vpn-connection-baby
     target: local
     cidr: 172.0.0.0/24
 
-# Delete peer CIDR from VPN connection
-- ic_is_vpn_cidr:
+- name: Delete peer CIDR from VPN connection
+  ic_is_vpn_cidr:
     gateway: ibmcloud-vpn-gateway-baby
     connection: ibmcloud-vpn-connection-baby
     target: peer
     cidr: 10.0.0.0/24
     state: absent
 
-# Delete local CIDR from VPN connection
-- ic_is_vpn_cidr:
+- name: Delete local CIDR from VPN connection
+  ic_is_vpn_cidr:
     gateway: ibmcloud-vpn-gateway-baby
     connection: ibmcloud-vpn-connection-baby
     target: local
@@ -118,13 +122,13 @@ def run_module():
     gateway = module.params['gateway']
     connection = module.params['connection']
     target = module.params['target']
-    name = module.params['cidr']
+    cidr = module.params['cidr']
     state = module.params["state"]
 
     # Split CIDR to get address and length
-    if name:
-        prefix_address = name.split('/')[0]
-        prefix_length = name.split('/')[1]
+    if cidr:
+        prefix_address = cidr.split('/')[0]
+        prefix_length = cidr.split('/')[1]
 
     if target == "local":
         check = vpn.check_vpn_gateway_local_cidr(
@@ -134,34 +138,29 @@ def run_module():
           gateway, connection, prefix_address, prefix_length)
 
     if state == "absent":
-        if "errors" in check:
-            for key in check["errors"]:
-                if key["code"] != "vpn_connection_cidr_not_found":
-                    module.fail_json(msg=check["errors"])
-                else:
-                    module.exit_json(changed=False, msg=(
-                        "{} cidr {} doesn't exist in connection {}".format(
-                          target, name, connection)))
+        if "peer_cidr" in check or "local_cidr" in check:
+            if target == "local":
+                result = vpn.remove_local_cidr(gateway, connection,
+                                               prefix_address, prefix_length)
+            else:
+                result = vpn.remove_peer_cidr(gateway, connection,
+                                              prefix_address, prefix_length)
 
-        if target == "local":
-            result = vpn.remove_local_cidr(gateway, connection,
-                                           prefix_address, prefix_length)
-        else:
-            result = vpn.remove_peer_cidr(gateway, connection,
-                                          prefix_address, prefix_length)
+            if "errors" in result:
+                module.fail_json(msg=result)
 
-        module.exit_json(changed=True, msg=(
-            "{} cidr {} successfully removed from connection {}".format(
-              target, name, connection)))
+            payload = {"cidr": cidr, "target": target,
+                       "connection": connection, "status": "deleted"}
+            module.exit_json(changed=True, msg=payload)
+
+        payload = {"cidr": cidr, "target": target,
+                   "connection": connection, "status": "not_found"}
+        module.exit_json(changed=False, msg=payload)
     else:
         if "peer_cidr" in check or "local_cidr" in check:
-            module.exit_json(change=False,
-                             msg="{} cidr {} already exists in connection"
-                                 " {}.".format(target, name, connection))
-        elif "errors" in check:
-            for key in check["errors"]:
-                if key["code"] != "vpn_connection_cidr_not_found":
-                    module.fail_json(msg=check["errors"])
+            payload = {"cidr": cidr, "target": target,
+                       "connection": connection, "status": "already_exists"}
+            module.exit_json(change=False, msg=payload)
 
         if target == "local":
             result = vpn.add_local_cidr_connection(
@@ -179,7 +178,7 @@ def run_module():
             )
 
         if "errors" in result:
-            module.fail_json(msg=result["errors"])
+            module.fail_json(msg=result)
 
         module.exit_json(changed=True, msg=result)
 
