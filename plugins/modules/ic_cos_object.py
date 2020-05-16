397569d4ed9448adee6810be2d8aad9d1e94aeaa
Gaëtan Trellu (goldyfruit) <gaetan.trellu@ibm.com>
2020-05-16 13:41:26 -0400

    [ic_cos_object] Small refact and documentation update

--- plugins/modules/ic_cos_object.py
+++ plugins/modules/ic_cos_object.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 # GNU General Public License v3.0+
+# (see COPYING or https://www.gnu.org/licenses/gpl-3.0.txt)
+
 
 from ansible.module_utils.basic import AnsibleModule
-from ibmcloud_python_sdk.cis.storage import object_storage as sdk
+from ibmcloud_python_sdk.cis.storage import object as sdk
+
 
 ANSIBLE_METADATA = {
     'metadata_version': '1.1',
@@ -14,39 +18,48 @@ ANSIBLE_METADATA = {
 DOCUMENTATION = r'''
 ---
 module: ic_cos_object
-short_description: Manage buckets and objects.
+short_description: Manage COS (Cloud Object Storage) objects on IBM Cloud.
 author: James Regis (@jregis)
 version_added: "2.9"
 description:
-    - Manage buckets and cloud objects on IBM Cloud.
+  - This module create or delete COS objects.
+  - By default the module will look for an existing service instance associated
+    to the Cloud Object Storage.
 requirements:
-    - "ibmcloud-python-sdk"
+  - "ibmcloud-python-sdk"
 options:
-    bucket: 
-        description: Bucket's name.
-        required: true
-    body: 
-        description: Object's content. Mutually exclusive with **path**.
-        required: false
-    path:
-        description: File's path to upload in the bucket.Mutually exclusive 
-        with **body**
-        required: false.
-    key:
-        description: Object key for which the PUT operation was initiated.
-        Typically, the object's name.
-    mode:
-        description: Replication mode of the buckets.
-        required: true
-    location:
-        description:
-            -  Geographic bucket location.
-        required: true        
-    service_instance:
-        description:
-            -  Name or UUID of the service_instance associated with the cloud 
-               object storage.
-        required: true
+  bucket:
+    description:
+      - Bucket name.
+    type: str
+    required: true
+  body:
+    description:
+      - Content to store into the object.
+      - Mutually exclusive with C(path) argument.
+    type: str
+  path:
+    description:
+      - Path to the file to upload.
+      - Mutually exclusive with C(body) argument.
+    type: str
+  object:
+    description:
+      - Name to set once the object is uploaded.
+    type: str
+    required: true
+  mode:
+    description:
+      - Replication mode of the buckets.
+    type: str
+  location:
+    description:
+      - Geographic bucket location.
+    type: str
+  service_instance:
+    description:
+      - Name or GUID of the service instance.
+    type: str
   state:
     description:
       - Should the resource be present or absent.
@@ -56,73 +69,74 @@ options:
 '''
 
 EXAMPLES = r'''
-# Put a object with "this is a content" as content in a bucket
-- ic_cos_object:
-    body: 'this is a content' 
-    key: it-s-an-object-baby
-    bucket: ibm-bucket-baby
+- name: Upload a file with a custom content.
+  ic_cos_object:
+    bucket: ibmcloud-bucket-baby
+    object: ibmcloud-object-baby
+    body: "This is my IBM Cloud content, baby!"
+
+- name: Upload an object with a custom content with specific information
+  ic_cos_object:
+    bucket: ibmcloud-bucket-baby
+    object: ibmcloud-object-baby
+    body: "This is my IBM Cloud content, baby!"
     mode: regional
     location: us-south
-    service_instance: my-service-instance-baby
+    service_instance: ibmcloud-resource-instance-baby
 
-# Upload a file to a bucket
-- ic_cos_object:
-    path: '/home/duke/berserk.avi' 
-    key: it-s-a-video-baby
-    bucket: ibm-bucket-baby
-    mode: regional
-    location: us-south
-    service_instance: my-service-instance-baby
+- name: Upload an existing file
+  ic_cos_object:
+    bucket: ibmcloud-bucket-baby
+    object: ibmcloud-object-baby
+    path: /home/ibmcloud/download/watson.png
 
-# Delete an object in a bucket
-- ic_cos_object:
-    key: it-s-an-object-baby
-    bucket: ibm-bucket-baby
-    mode: regional
-    location: us-south
-    service_instance: my-service-instance-baby
-
+- name: Delete object from a bucket
+  ic_cos_object:
+    bucket: ibmcloud-bucket-baby
+    object: ibmcloud-object-baby
 '''
+
 
 def run_module():
     module_args = dict(
         body=dict(
             type='str',
             required=False),
-        key=dict(
+        object=dict(
             type='str',
-            required=True),
+            required=False),
         path=dict(
             type='str',
-            required=False),    
+            required=False),
         bucket=dict(
             type='str',
             required=True),
         acl=dict(
             type='str',
-            choices=['private','public-read','public-read-write',
-            'authenticated-read','aws-exec-read','bucket-owner-read',
-            'bucket-owner-full-control'],
+            choices=['private', 'public-read', 'public-read-write',
+                     'authenticated-read', 'aws-exec-read',
+                     'bucket-owner-read', 'bucket-owner-full-control'],
             required=False),
         mode=dict(
             type='str',
-            choices=['regional', 'direct_regional', 'cross_region', 
-            'direct_us_cross_region', 'direct_eu_cross_region', 
-            'direct_ap_cross_region', 'single_data_center', 
-            'direct_single_data_center'],
-            required=True),
+            choices=['regional', 'direct_regional', 'cross_region',
+                     'direct_us_cross_region', 'direct_eu_cross_region',
+                     'direct_ap_cross_region', 'single_data_center',
+                     'direct_single_data_center'],
+            required=False),
         location=dict(
             type='str',
             choices=['us-south', 'us-east', 'eu-united-kingdom', 'eu-germany',
-            'ap-autralia', 'ap-japan', 'us-cross-region', 'eu-cross-region',
-            'ap-cross-region', 'us', 'dallas', 'san-jose', 'eu', 'amsterdam',
-            'frankfurt', 'milan', 'ap', 'tokyo', 'seoul', 'hong-kong',
-            'chennai', 'melbourne', 'mexico', 'montreal', 'oslo', 'paris', 
-            'sao-paulo', 'seoul', 'singapore', 'toronto'],
-            required=True),
+                     'ap-autralia', 'ap-japan', 'us-cross-region',
+                     'eu-cross-region', 'ap-cross-region', 'us', 'dallas',
+                     'san-jose', 'eu', 'amsterdam', 'frankfurt', 'milan', 'ap',
+                     'tokyo', 'seoul', 'hong-kong', 'chennai', 'melbourne',
+                     'mexico', 'montreal', 'oslo', 'paris', 'sao-paulo',
+                     'seoul', 'singapore', 'toronto'],
+            required=False),
         service_instance=dict(
             type='str',
-            required=True),
+            required=False),
         state=dict(
             type='str',
             default='present',
@@ -135,9 +149,7 @@ def run_module():
         supports_check_mode=False
     )
 
-    object_storage = sdk.ObjectStorage()
-
-    obj_key = module.params['key']
+    object = module.params['object']
     path = module.params['path']
     body = module.params['body']
     acl = module.params['acl']
@@ -146,79 +158,54 @@ def run_module():
     location = module.params['location']
     service_instance = module.params["service_instance"]
     state = module.params["state"]
-    
-    check = object_storage.get_bucket(
-            bucket=bucket,
-            mode=mode,
-            location=location,
-            service_instance=service_instance)
 
-    if "errors" in check:
-        for key in check["errors"]:
-            if key["code"] == "not_found":
-                module.exit_json(changed=False, msg=(
-                    "bucket {} should exist before any operation.".format(
-                                bucket)))
+    sdk_object = sdk.ObjectStorage(
+                    mode=mode,
+                    location=location,
+                    service_instance=service_instance
+                )
+
+    if path and body:
+        msg = "path and body are mutually exclusive"
+        module.fail_json(changed=False, msg=msg)
+
+    check = sdk_object.get_object(bucket, object)
+
     if state == "absent":
-        
-        search = object_storage.get_object(
-            key=obj_key,
-            bucket=bucket,
-            mode=mode,
-            location=location,
-            service_instance=service_instance,
-        )
-        if "errors" in search:
-            module.exit_json(changed=False, msg=(
-                "object {} doesn't exist".format(obj_key)))
+        if "Key" in check:
+            result = sdk_object.delete_object(bucket, object)
+            if "errors" in result:
+                module.fail_json(msg=result)
 
-        delete_result = object_storage.delete_object(
-            key=obj_key,
-            bucket=bucket,
-            mode=mode,
-            location=location,
-            service_instance=service_instance
-        )     
+            payload = {"object": object, "bucket": bucket, "status": "deleted"}
+            module.exit_json(changed=True, msg=payload)
 
-        if "status" in delete_result:
-            if delete_result["status"] == "deleted":
-                payload = {"msg": "object {} successfully deleted".format(obj_key)}
-                module.exit_json(changed=True, msg=(payload))
-        module.fail_json(msg=delete_result)
-
-    else: 
-        if path is not None and body is not None:
-            module.fail_json(changed=False, msg=(
-                "path and body are mutually exclusive"))
-
-        if path is not None:
-            result = object_storage.upload_file(
-                key=obj_key,
+        payload = {"object": object, "bucket": bucket, "status": "not_found"}
+        module.exit_json(changed=False, msg=payload)
+    else:
+        result = None
+        if path:
+            result = sdk_object.upload_file(
+                key=object,
                 bucket=bucket,
                 path=path,
-                acl=acl,
-                mode=mode,
-                location=location,
-                service_instance=service_instance)
+                acl=acl
+            )
 
             if "errors" in result:
-                module.fail_json(msg=result["errors"])
-            module.exit_json(changed=True, msg=(result))
-
-        if body is not None:
-            result = object_storage.put_object(
-                key=obj_key,
+                module.fail_json(msg=result)
+        elif body:
+            result = sdk_object.put_object(
+                key=object,
                 bucket=bucket,
                 body=body,
-                acl=acl,
-                mode=mode,
-                location=location,
-                service_instance=service_instance)
-
+                acl=acl
+            )
             if "errors" in result:
-                module.fail_json(msg=result["errors"])
-            module.exit_json(changed=True, msg=(result))
-    module.exit_json(changed=False, msg=(check))
+                module.fail_json(msg=result)
+
+        module.exit_json(changed=False, msg=result)
+
 
 def main():
     run_module()
